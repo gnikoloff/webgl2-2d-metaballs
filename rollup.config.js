afd711aa8c16616b5ab7fd2ce2ee7364603ad9dd
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-01-03 16:21:38 +0100

    chnged build settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import css from 'rollup-plugin-css-only'
+import copy from 'rollup-plugin-copy'
+
 
 export default {
   input: 'app/script.js',
@@ -7,6 +9,13 @@ export default {
     format: 'iife'
   },
   plugins: [
+    copy({
+      targets: [
+        { src: 'index.html', dest: 'dist' },
+        { src: 'css', dest: 'dist' },
+        { src: 'favicon.ico', dest: 'dist' }
+      ]
+    }),
     css({ output: 'bundle.css' })
   ]
 }
